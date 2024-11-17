c9a92a59622ba10bdac1dd9d61aceee82fd4c13f
Akash Jaiswal <akashjais929@gmail.com>
2024-11-17 09:38:34 +0530

    Improved sign-in using clerk and auth

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,27 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  // console.log(profile);
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
   return <div>Create a Server</div>;
 };
 
