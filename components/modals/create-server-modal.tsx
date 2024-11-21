6f8a730c198f9f20b7e0b2c09027cb2ccdc52e8d
Akash Jaiswal <akashjais929@gmail.com>
2024-11-21 23:34:40 +0530

    Added create server store using zustand and functionality of adding new servers

---
+++ components/modals/create-server-modal.tsx
@@ -0,0 +1,139 @@
+"use client";
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import FileUpload from "@/components/file-upload";
+import { serverCreationSchema } from "@/utils/schema";
+import axios, { AxiosError } from "axios";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-model-store";
+
+const CreateServerModal = () => {
+  const { isOpen, onClose, type } = useModal();
+  const { toast } = useToast();
+  const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(serverCreationSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isModalOpen = isOpen && type === "createServer";
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  const { isSubmitting: isLoading } = form.formState;
+
+  const onSubmit = async (formData: z.infer<typeof serverCreationSchema>) => {
+    try {
+      await axios.post("/api/server", {
+        name: formData.name,
+        imageUrl: formData.imageUrl,
+      });
+
+      form.reset();
+      router.refresh();
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      console.error(axiosError);
+      // Handle error based on the specific API error
+      toast({
+        title: "Error",
+        description: axiosError.response?.data.message,
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Create your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image, which you
+            can always change
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center  justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                name="name"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                      Name :{" "}
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        disabled={isLoading}
+                        placeholder="Enter a name for your server..."
+                        {...field}
+                        type="text"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button disabled={isLoading} variant={"primary"}>
+                Create Server
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateServerModal;
